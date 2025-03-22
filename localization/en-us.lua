3575a058fe6afac8f10edaa37d047232647db9d7
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-03-22 01:32:15 -0400

    Initial commit

---
+++ localization/en-us.lua
@@ -0,0 +1,6 @@
+return {
+    descriptions = {
+        Joker = {}
+    },
+    misc = {}
+}
---
+++ src/main.lua
@@ -0,0 +1,6 @@
+SMODS.Atlas {
+    key = "Jokers",
+    path = "Jokers.png",
+    px = 71,
+    py = 95
+}
